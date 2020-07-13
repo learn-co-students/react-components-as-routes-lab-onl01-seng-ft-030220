70d6868491e527ea8b87df0815197d4531bdc4f6
matt_kuras <mattkuras@gmail.com>
2020-07-13 14:25:01 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,3 +1,31 @@
+// import React from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from 'react-router-dom';
+// import Home from '../components/Home';
+// import NavBar from '../components/NavBar';
+// import Actors from '../components/Actors';
+// import Directors from '../components/Directors';
+// import Movies from '../components/Movies';
+
+
+// const App = (props) => {
+//   return (
+//     <Router>
+//        <div className="app">
+//       <Navbar />
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/movies" component={Movies} />
+//       <Route exact path="/directors" component={Directors} />
+//       <Route exact path="/actors" component={Actors} />
+//     </div>
+//     </Router>
+//   );
+// };
+
+// export default App
+  
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -13,7 +41,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div className="app">
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/movies" component={Movies} />
+      </div>
     </Router>
   );
 };
